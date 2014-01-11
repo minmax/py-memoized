a65bc288142e151ccfbcfd59d8539e8656234458
Karataev Pavel <minmax777@gmail.com>
2014-01-12 00:28:49 +0400

    Remove unused code

--- memoized/memoizer.py
+++ memoized/memoizer.py
@@ -2,29 +2,6 @@ from .storage_factory_container import storage_factory_container
 from .errors import NoResult
 
 
-def default_key(*args, **kwargs):
-    if args or kwargs:
-        raise TypeError('Args or KWargs not supported')
-
-
-class CountingInvalidateFactory(object):
-    pass
-
-
-class Options(object):
-    def __init__(self, function, storage, key, invalidate):
-        self.function = function
-        self.storage = storage
-        self.key = key
-        self.invalidate = invalidate
-
-
-class KeyGenerator(object):
-    def __init__(self, key=None):
-        if key is None:
-            self.key = None
-
-
 class Memoizer(object):
     def __init__(self, function, storage, **kwargs):
         self.function = function
